45d8b95a5ceb202def4110968b83ee1368e3a4c6
mkchalla <44621173+mkchalla@users.noreply.github.com>
2024-02-17 17:42:45 +1100

    initial commit

---
+++ src/main/java/com/mkc/spatial/springpostgisgeometries/SpringPostgisGeometriesApplication.java
@@ -0,0 +1,13 @@
+package com.mkc.spatial.springpostgisgeometries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgisGeometriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostgisGeometriesApplication.class, args);
+	}
+
+}
